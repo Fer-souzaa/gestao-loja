be547aa113ca7ad7771e76befc1c89af8d31f9ce
Fernando Santos Souza <fernandorr.fs@gmail.com>
2026-02-17 18:07:19 -0400

    Add ForceJsonMiddleware and improve exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middleware\ForceJsonMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Base\Traits\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(ForceJsonMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $throwable) {
+            return Response::returnError($throwable);
+        });
     })->create();
